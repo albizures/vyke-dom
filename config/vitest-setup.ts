3cd3e027e5fdd6d3648ba730c2dcc56c65e6e87d
Jose Albizures <jose@albizures.com>
2024-07-10 21:45:30 -0600

    chore: fix lint issue

--- config/vitest-setup.ts
+++ config/vitest-setup.ts
@@ -6,7 +6,7 @@ declare module 'vitest' {
 	// eslint-disable-next-line ts/consistent-type-definitions
 	interface JestAssertion<T = any>
 		extends TestingLibraryMatchers<
-      ReturnType<typeof expect.stringContaining>,
-      T
-    > {}
+			ReturnType<typeof expect.stringContaining>,
+			T
+		> {}
 }
